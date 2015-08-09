5be46fc7b55ea324d16e287c8b45a688b2cc2d8a
viagrawal <viagrawal@expedia.com>
2015-08-09 08:56:43 +0530

    Formatting

--- src/RightDownPaths.java
+++ src/RightDownPaths.java
@@ -4,16 +4,14 @@
  */
 public class RightDownPaths {
 
-	
 	int[][] arr;
 	int length;
-	
-	
-	RightDownPaths(){
-		arr=new int[4][4];
-		this.length=4;
+
+	RightDownPaths() {
+		arr = new int[4][4];
+		this.length = 4;
 	}
-	
+
 	/**
 	 * @param args
 	 */
@@ -22,55 +20,47 @@ public class RightDownPaths {
 
 	}
 
-	
-	public void operate(){
+	public void operate() {
 		this.calculate(arr);
 	}
-	
-	
-	
-	
-	public void calculate(int[][] arr){
-		
+
+	public void calculate(int[][] arr) {
+
 		System.out.println("initial array :");
 		printArray(arr);
-		
-		
-		int m=length-1;
-		
-		arr[m][m]=0;
-		
-		for(int k=0;k<length;k++){
-			arr[k][m]=1;
-			arr[m][k]=1;
+
+		int m = length - 1;
+
+		arr[m][m] = 0;
+
+		for (int k = 0; k < length; k++) {
+			arr[k][m] = 1;
+			arr[m][k] = 1;
 		}
-		
-		
-		for(int i=m-1; i>=0;i--){
-			
-			for(int j=m-1;j>=0;j--){
-						
-				arr[i][j]=arr[i+1][j]+arr[i][j+1];
+
+		for (int i = m - 1; i >= 0; i--) {
+			for (int j = m - 1; j >= 0; j--) {
+				arr[i][j] = arr[i + 1][j] + arr[i][j + 1];
 			}
-			
 		}
 		System.out.println("Array after processing");
 		printArray(arr);
-		
-		
-		System.out.println("Total number of paths going right/down from [0,0] is = "+arr[0][0]);
-		
+
+		System.out
+				.println("Total number of paths going right/down from [0,0] is = "
+						+ arr[0][0]);
+
 	}
-	
-	public void printArray(int [][] arr){	
-		int m=length;
-		
-		for(int i=0;i<m;i++){
-			for(int j=0;j<m;j++){
-				System.out.print(arr[i][j]+" ");
+
+	public void printArray(int[][] arr) {
+		int m = length;
+
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < m; j++) {
+				System.out.print(arr[i][j] + " ");
 			}
 			System.out.println(" ");
 		}
 	}
-	
+
 }
--- src/VerifyBST.java
+++ src/VerifyBST.java
@@ -1,71 +1,68 @@
 /**
- * @author Vikky.Agrawal
+ * @author vikky.agrawal
  * 
  */
 
 public class VerifyBST {
 
-	
-	private TreeNode root ;
-	
-	private static final int MIN=Integer.MIN_VALUE;
-	private static final int MAX=Integer.MAX_VALUE;
+	private TreeNode root;
+
+	private static final int MIN = Integer.MIN_VALUE;
+	private static final int MAX = Integer.MAX_VALUE;
 
 	public VerifyBST() {
 		root = new TreeNode(4);
 	}
-	
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		VerifyBST obj=new VerifyBST();
+		VerifyBST obj = new VerifyBST();
 		obj.operate();
 	}
 
 	public void operate() {
-				
-				//let's construct a random binary tree
-				//   4
-				// 2   6
-				//1 3 5 7
-			 	root.setLeft( new TreeNode(2));
-				root.setRight( new TreeNode(6));
-				root.getLeft().setLeft( new TreeNode(1));
-				root.getLeft().setRight( new TreeNode(3));
-				root.getRight().setLeft( new TreeNode(5));
-				root.getRight().setRight( new TreeNode(7));
-				
-				//test by changing values
-				root.getLeft().setRight( new TreeNode(6));
-				
-				inorder(root);
-				
-				System.out.println("Whether this tree is BST? :  "+checkBST(root, MIN, MAX));
+
+		// let's construct a random binary tree
+		// 4
+		// 2 6
+		// 1 3 5 7
+		root.setLeft(new TreeNode(2));
+		root.setRight(new TreeNode(6));
+		root.getLeft().setLeft(new TreeNode(1));
+		root.getLeft().setRight(new TreeNode(3));
+		root.getRight().setLeft(new TreeNode(5));
+		root.getRight().setRight(new TreeNode(7));
+
+		// test by changing values
+		root.getLeft().setRight(new TreeNode(6));
+
+		inorder(root);
+
+		System.out.println("Whether this tree is BST? :  "
+				+ checkBST(root, MIN, MAX));
 	}
-	
-	//Duplicates are not handled in given function.
-	
-	public boolean checkBST(TreeNode root, int MIN, int MAX){
-		
-		if(root==null){
+
+	// Duplicates are not handled in given function.
+
+	public boolean checkBST(TreeNode root, int MIN, int MAX) {
+
+		if (root == null) {
 			return true;
 		}
-		
-		if(root.getData()>MIN && root.getData()< MAX){
-			return checkBST(root.getLeft(), MIN,root.getData()) && checkBST(root.getRight(), root.getData(), MAX);
-		}	
-		
+
+		if (root.getData() > MIN && root.getData() < MAX) {
+			return checkBST(root.getLeft(), MIN, root.getData())
+					&& checkBST(root.getRight(), root.getData(), MAX);
+		}
+
 		return false;
 	}
-	
-	
-	
-	
+
 	// Helper functions for tree
-	
-	/*
+
+	/**
 	 * Recursive in-order traversal
 	 */
 	public void inorder(TreeNode root) {
@@ -75,8 +72,7 @@ public class VerifyBST {
 			inorder(root.getRight());
 		}
 	}
-	
-	
+
 	// DS for tree
 	private static class TreeNode {
 
