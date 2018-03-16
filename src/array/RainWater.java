9eca9a412a2b2653699373f03a234737a329a651
Vikky <vikkyagrawal87@gmail.com>
2018-03-17 00:07:47 +0530

    practice

---
+++ src/array/RainWater.java
@@ -0,0 +1,50 @@
+package array;
+
+import static java.lang.Math.max;
+
+/**
+ * Trapping Rain Water within given set of bars
+ * In trapping rain water problem, we need to find the maximum amount of water
+ * that can be trapped within given set of bars where width of each bar is 1 unit.
+ * <p>
+ * Created by viagrawal on 3/16/18.
+ */
+public class RainWater
+{
+
+	/**
+	 * O(n) with O(1) time complexity
+	 * Idea is to find out max left and max right, minimum of these 2 minus bar height will give the water storage
+	 * Maintain 2 pointers for maximum in left and maximum in right
+	 * @param args
+	 */
+
+	public static void main(String[] args)
+	{
+
+		int[] bars = { 7, 0, 4, 2, 5, 8, 6, 4, 0, 5 };
+		int length = bars.length;
+		int left = 0, right = length - 1;
+		int maxLeft = bars[left];
+		int maxRight = bars[right];
+		int water = 0;
+
+		while (left < right)
+		{
+			if (bars[left] <= bars[right])
+			{
+				left++;
+				maxLeft = max(maxLeft, bars[left]);
+				water += (maxLeft - bars[left]);
+			}
+			else
+			{
+				right--;
+				maxRight = max(maxRight, bars[right]);
+				water += (maxRight - bars[right]);
+			}
+		}
+		System.out.println("max water that can be trapped : " + water);
+	}
+
+}
--- src/search/BinarySearch.java
+++ src/search/BinarySearch.java
@@ -10,10 +10,46 @@ public class BinarySearch
 	{
 		BinarySearch obj = new BinarySearch();
 		int[] arr = { 1, 2, 3, 4, 5 };
+		int[] rotatedArray = { 2, 3, 4, 6, 8, 1 };
 
-		for (int i : arr)
-			System.out.println(obj.binarySearch(arr, i, 0, arr.length - 1));
+		for (int i : rotatedArray)
+		{
+			//			System.out.println(obj.binarySearch(arr, i, 0, arr.length - 1));
+			System.out.print(obj.binarySearchRotated(rotatedArray, i, 0, rotatedArray.length - 1) + " ");
+		}
 
+	}
+
+	/**
+	 * Search in circularly rotated array, since array is sorted use binary search functionality
+	 *
+	 * @param arr
+	 * @param data
+	 * @param low
+	 * @param high
+	 * @return
+	 */
+	private int binarySearchRotated(int[] arr, int data, int low, int high)
+	{
+		int left = arr[low];
+		int right = arr[high];
+		if (data == left)
+			return low;
+		if (data == right)
+			return high;
+
+		if ((data < left && data < right) || (data > left && data > right))
+		{
+			int mid = (low + high) / 2;
+			if (data == arr[mid])
+				return mid;
+			if (data < arr[mid])
+				return binarySearchRotated(arr, data, low, mid - 1);
+			else
+				return binarySearchRotated(arr, data, mid + 1, high);
+		}
+		else
+			return binarySearch(arr, data, low, high);
 	}
 
 	public int binarySearch(int[] arr, int data, int p, int r)
